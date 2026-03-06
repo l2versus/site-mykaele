7bb76ed2f7a1333ce8d43b1e124354f44d07589a
l2versus <contactl2verus@gmail.com>
2026-03-05 23:12:16 -0300

    commit completo

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,26 +4,23 @@ import pg from 'pg'
 
 const { Pool } = pg
 
-// Configura a conexão nativa com a URL do seu .env
 const pool = new Pool({ 
   connectionString: process.env.DATABASE_URL 
 })
 
-// Cria o adaptador do Prisma 7
 const adapter = new PrismaPg(pool)
 
-// Função que cria o cliente já com o adaptador
 const prismaClientSingleton = () => {
   return new PrismaClient({ adapter })
 }
 
-// Previne múltiplas conexões abertas no modo de desenvolvimento (Hot Reload)
 declare global {
   var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>
 }
 
 const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
 
-export default prisma
+export { prisma }          // named export  ← isso resolve o erro
+export default prisma      // mantém o default também
 
 if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
